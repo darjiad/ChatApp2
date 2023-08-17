e3dd7ad64a421d4f9964ae9b6a4312584d2b9a17
Aditi Darji <adudarji2002@gmail.com>
2023-08-17 15:07:25 +0530

    Model add

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username:"Ayesha",password:"ayesha")
+User.create(username:"Mahi",password:"mahi")
+User.create(username:"Ekta",password:"ekta")
+User.create(username:"Honey",password:"honey")
+User.create(username:"Jin",password:"jin")
+
