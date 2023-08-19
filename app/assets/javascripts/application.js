110d54c206fa681246a68449433339dfac7571f8
Aditi Darji <adudarji2002@gmail.com>
2023-08-19 11:08:03 +0530

    Final App

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,6 +16,25 @@
 //= require turbolinks
 //= require semantic-ui
 //= require_tree .
+
+scroll_botton= function()
+{
+  if($('#message').length >0)
+  {
+    $('#message').scrollTop($('#message')[0].scrollHeight)
+  }
+}
+submit_message=function()
+{
+  $('#message_body').on('keydown',function(e){
+    if(e.keyCode==13)
+    {
+      $('button').click();
+      e.target.value="";
+    }
+  })
+}
+
 $(document).on('turbolinks:load',function(){
     $('.ui.dropdown').dropdown();
     $('.message .close')
@@ -24,7 +43,8 @@ $(document).on('turbolinks:load',function(){
       .closest('.message')
       .transition('fade')
     ;
-  })
-;
-;
+  });
+submit_message();
+scroll_botton();
+
 })
